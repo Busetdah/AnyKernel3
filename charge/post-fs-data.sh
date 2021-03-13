f77cbc393fd79ddb1987de710888c675be04740e
Aprianto WPJ <apriantowpj@gmail.com>
2021-03-13 13:56:32 +0700

    Delete charge directory

--- charge/post-fs-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-chmod 755 /sys/class/power_supply/*/*
-while true; do
-Write '5100000' > /sys/class/power_supply/battery/input_current_max
-Write '5100000' > /sys/class/power_supply/battery/constant_charge_current
-Write '5100000' > /sys/class/power_supply/battery/constant_charge_current_max
-sleep 10
